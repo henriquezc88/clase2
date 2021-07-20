d91db886e602f7a8694c491b15402baa9360c3a1
henri <henri@DESKTOP-IR8L66E>
2021-07-20 16:48:27 -0600

    Agregar archivos de proyecto.

---
+++ clase2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace clase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            double costo=0, cliente=0, cambio=0;
+            Console.WriteLine("ingrese el costo del articulo:");
+            costo = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese la cantidad entregada por el cliente:");
+            cliente = double.Parse(Console.ReadLine());
+            cambio = cliente - costo;
+            Console.WriteLine("el total a pagar es:" + cambio);
+       
+        }
+    }
+}
